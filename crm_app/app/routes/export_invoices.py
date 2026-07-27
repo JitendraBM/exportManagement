6876b36bb18549392c3695a110f72e92e0c03b52
tdpatelgit <tdpatelmail@gmail.com>
2026-07-27 19:22:24 +0530

    fix: now FOB and CNF values are auto calculated

--- crm_app/app/routes/export_invoices.py
+++ crm_app/app/routes/export_invoices.py
@@ -35,7 +35,7 @@ _HEADER_FIELDS = [
     "port_of_loading", "port_of_discharge", "final_destination", "nature_of_contract", "payment_terms",
     "buyer_order_no", "buyer_order_date",
     "export_under", "epcg_number", "epcg_date", "loading_type", "tax_mode", "exchange_rate",
-    "sea_freight", "insurance", "certification", "other_charges", "discount_amount", "fob_value", "cnf_value",
+    "sea_freight", "insurance", "certification", "other_charges", "discount_amount",
     "bank_name", "bank_account_number", "bank_ifsc_code", "bank_swift_code", "bank_branch", "bank_address",
     "authorised_person_name", "authorised_person_designation", "self_sealing_declaration",
     "examination_date", "location_code_08b", "booking_no", "issuing_authority", "issuing_authority_address",
